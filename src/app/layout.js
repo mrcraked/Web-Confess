d22de23d720b0b6f0bf03c791d7635c76069a391
mrcraked <89685779+mrcraked@users.noreply.github.com>
2024-12-18 20:26:39 +0900

    ok

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,8 +20,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        
       >
         {children}
       </body>
